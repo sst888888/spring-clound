a7aaf809ad0984f22d64119931e10681206ec641
cp <4525999888@qq.com>
2023-09-19 20:06:36 +0800

    全局降级

--- micro-service-cloud-consumer-dept-feign/src/main/java/com/example/microservicecloudconsumerdeptfeign/controller/HystrixController_Consumer.java
+++ micro-service-cloud-consumer-dept-feign/src/main/java/com/example/microservicecloudconsumerdeptfeign/controller/HystrixController_Consumer.java
@@ -1,6 +1,7 @@
 package com.example.microservicecloudconsumerdeptfeign.controller;
 
 import com.example.microservicecloudconsumerdeptfeign.service.DeptHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,32 +13,51 @@ import javax.annotation.Resource;
 /**
  * @author: cp
  * @date: 2023-09-19 19:38
+ * @ link http://c.biancheng.net/springcloud/hystrix.html
  */
 
 @Slf4j
 @RestController
+@DefaultProperties(defaultFallback = "dept_Global_FallbackMethod") //全局的服务降级方法
 public class HystrixController_Consumer {
 
 
     @Resource
     private DeptHystrixService deptHystrixService;
+
+
     @RequestMapping(value = "/consumer/dept/hystrix/ok/{id}")
     public String deptInfo_Ok(@PathVariable("id") Integer id) {
         return deptHystrixService.deptInfo_Ok(id);
     }
+
+
+
     //在客户端进行降级
     @RequestMapping(value = "/consumer/dept/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "dept_TimeoutHandler") //为该请求指定专属的回退方法
+//    @HystrixCommand(fallbackMethod = "dept_TimeoutHandler") //为该请求指定专属的回退方法
+    @HystrixCommand // 全局
     public String deptInfo_Timeout(@PathVariable("id") Integer id) {
         String s = deptHystrixService.deptInfo_Timeout(id);
         System.out.println(s);
         return s;
     }
+
+
     // deptInfo_Timeout方法的 专用 fallback 方法
     public String dept_TimeoutHandler(@PathVariable("id") Integer id) {
         System.out.println("deptInfo_Timeout 出错，服务已被降级！");
         return "服务端系统繁忙，请稍后再试！（客户端 deptInfo_Timeout 专属的回退方法触发）";
     }
 
+
+    /**
+     * 全局的 fallback 方法，
+     * 回退方法必须和 hystrix 的执行方法在相同类中
+     * @DefaultProperties(defaultFallback = "dept_Global_FallbackMethod") 类上注解，请求方法上使用 @HystrixCommand 注解
+     */
+    public String dept_Global_FallbackMethod() {
+        return "运行出错或服务端系统繁忙，请稍后再试！（客户端全局回退方法触发）";
+    }
 
 }
