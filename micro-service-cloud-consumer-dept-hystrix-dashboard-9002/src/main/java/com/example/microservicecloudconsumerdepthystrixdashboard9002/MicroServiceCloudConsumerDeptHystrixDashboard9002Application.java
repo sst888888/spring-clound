8f26487c3afe553542e087550a34bf4c727415c9
cp <4525999888@qq.com>
2023-09-19 22:39:48 +0800

    dashbord

---
+++ micro-service-cloud-consumer-dept-hystrix-dashboard-9002/src/main/java/com/example/microservicecloudconsumerdepthystrixdashboard9002/MicroServiceCloudConsumerDeptHystrixDashboard9002Application.java
@@ -0,0 +1,15 @@
+package com.example.microservicecloudconsumerdepthystrixdashboard9002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class MicroServiceCloudConsumerDeptHystrixDashboard9002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCloudConsumerDeptHystrixDashboard9002Application.class, args);
+	}
+
+}
