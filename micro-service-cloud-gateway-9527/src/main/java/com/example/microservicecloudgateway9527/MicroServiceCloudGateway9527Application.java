968d580fea98d77c29c4c04a42980daba66ee6ba
cp <4525999888@qq.com>
2023-09-20 19:24:20 +0800

    add

---
+++ micro-service-cloud-gateway-9527/src/main/java/com/example/microservicecloudgateway9527/MicroServiceCloudGateway9527Application.java
@@ -0,0 +1,13 @@
+package com.example.microservicecloudgateway9527;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceCloudGateway9527Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceCloudGateway9527Application.class, args);
+	}
+
+}
