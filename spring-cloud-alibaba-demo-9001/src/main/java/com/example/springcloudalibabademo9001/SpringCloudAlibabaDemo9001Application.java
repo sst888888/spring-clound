5dcb19e867d5271bc13870f9bebe14731f2058b4
cp <4525999888@qq.com>
2023-09-21 15:54:11 +0800

    nacos

---
+++ spring-cloud-alibaba-demo-9001/src/main/java/com/example/springcloudalibabademo9001/SpringCloudAlibabaDemo9001Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabademo9001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaDemo9001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAlibabaDemo9001Application.class, args);
+	}
+
+}
