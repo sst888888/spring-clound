d7fdbd233df791542b1dcc5ae29a267f4f70c290
cp <4525999888@qq.com>
2023-09-21 17:03:15 +0800

    nacos provider

--- spring-cloud-alibaba-demo/spring-cloud-alibaba-provider-9001/src/main/java/com/example/springcloudalibabaprovider9001/SpringCloudAlibabaProvider9001Application.java
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-provider-9001/src/main/java/com/example/springcloudalibabaprovider9001/SpringCloudAlibabaProvider9001Application.java
@@ -2,8 +2,10 @@ package com.example.springcloudalibabaprovider9001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //开启服务发现功能
 public class SpringCloudAlibabaProvider9001Application {
 
 	public static void main(String[] args) {
