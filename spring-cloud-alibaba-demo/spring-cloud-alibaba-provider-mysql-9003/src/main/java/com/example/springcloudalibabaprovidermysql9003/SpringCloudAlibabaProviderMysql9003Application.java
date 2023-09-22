3f965c7548eadd5a53c1a9703e7f7360bc8c9f78
cp <4525999888@qq.com>
2023-09-22 13:26:41 +0800

    mysql

---
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-provider-mysql-9003/src/main/java/com/example/springcloudalibabaprovidermysql9003/SpringCloudAlibabaProviderMysql9003Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabaprovidermysql9003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaProviderMysql9003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAlibabaProviderMysql9003Application.class, args);
+	}
+
+}
---
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-provider-mysql-9003/src/test/java/com/example/springcloudalibabaprovidermysql9003/SpringCloudAlibabaProviderMysql9003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabaprovidermysql9003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaProviderMysql9003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
