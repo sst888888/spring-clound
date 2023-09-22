812b3582d7e9ba32b72558dcfa30a442fc83054e
cp <4525999888@qq.com>
2023-09-22 15:53:13 +0800

    9903

---
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-consumer-mysql-9903/src/test/java/com/example/springcloudalibabaconsumermysql9903/SpringCloudAlibabaConsumerMysql9903ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabaconsumermysql9903;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaConsumerMysql9903ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
