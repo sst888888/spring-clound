5f1592cc3c0e2cbc6abd56e0eba0ad90603645d2
cp <4525999888@qq.com>
2023-09-25 18:09:58 +0800

    sentinel gateway

---
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-sentinel-service-gateway-9402/src/main/java/com/example/springcloudalibabasentinelservicegateway9402/SpringCloudAlibabaSentinelServiceGateway9402Application.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabasentinelservicegateway9402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaSentinelServiceGateway9402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAlibabaSentinelServiceGateway9402Application.class, args);
+	}
+
+}
---
+++ spring-cloud-alibaba-demo/spring-cloud-alibaba-sentinel-service-gateway-9402/src/test/java/com/example/springcloudalibabasentinelservicegateway9402/SpringCloudAlibabaSentinelServiceGateway9402ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudalibabasentinelservicegateway9402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaSentinelServiceGateway9402ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
